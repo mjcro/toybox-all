515d5f4d2850ea4216846201f726646db8192ae6
Gotterdemarung <gotterdemarung@gmail.com>
2025-06-08 14:15:15 +0300

    :recycle: File binding rewritten using file chooser widget

--- templates/src/main/java/io/github/mjcro/toybox/templates/bindings/FileBinding.java
+++ templates/src/main/java/io/github/mjcro/toybox/templates/bindings/FileBinding.java
@@ -2,31 +2,15 @@ package io.github.mjcro.toybox.templates.bindings;
 
 import io.github.mjcro.interfaces.Decorator;
 import io.github.mjcro.toybox.api.Environment;
-import io.github.mjcro.toybox.swing.BorderLayoutMaster;
-import io.github.mjcro.toybox.swing.prefab.ToyBoxButtons;
-import io.github.mjcro.toybox.swing.prefab.ToyBoxTextComponents;
+import io.github.mjcro.toybox.swing.widgets.FileChooserInput;
 import lombok.NonNull;
 
-import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
-import java.awt.*;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.dnd.DnDConstants;
-import java.awt.dnd.DropTarget;
-import java.awt.dnd.DropTargetDropEvent;
-import java.awt.event.ActionEvent;
-import java.io.File;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.List;
 
 public class FileBinding extends AbstractJPanelContainerBinding {
-    private final Environment environment;
-    private final JTextField chosenFileNameTextField = ToyBoxTextComponents.createJTextField();
-    private final JButton chooseFileButton = ToyBoxButtons.create("Choose", this::onChooseFileButtonClick);
-    private final JButton clearButton = ToyBoxButtons.create("Clear", this::onClearButtonClick);
-    private File file;
-    private FileFilter[] fileFilters = null;
+    private final FileChooserInput fileChooserInput;
 
     public FileBinding(
             @NonNull Environment environment,
@@ -34,18 +18,19 @@ public class FileBinding extends AbstractJPanelContainerBinding {
             @NonNull Field field
     ) {
         super(target, field);
-        this.environment = environment;
 
         // Reading options
+        FileFilter[] fileFilters = new FileFilter[0];
         Class<?> options = annotation.options();
         if (options != null && options != Void.class) {
-            readOptions(options);
+            fileFilters = readOptions(options);
         }
 
-        initComponents();
+        this.fileChooserInput = new FileChooserInput(environment, "Choose file", this::fireSubmit, fileFilters);
+        super.add(this.fileChooserInput);
     }
 
-    private void readOptions(Class<?> options) {
+    private FileFilter[] readOptions(Class<?> options) {
         if (options.isEnum()) {
             Object[] constants = options.getEnumConstants();
             ArrayList<FileFilter> filters = new ArrayList<>();
@@ -59,85 +44,20 @@ public class FileBinding extends AbstractJPanelContainerBinding {
             }
 
             if (!filters.isEmpty()) {
-                fileFilters = filters.toArray(new FileFilter[0]);
+                return filters.toArray(new FileFilter[0]);
             }
         }
-    }
 
-    private void initComponents() {
-        JPanel buttons = new JPanel(new FlowLayout(FlowLayout.LEFT, 1, 0));
-        buttons.add(chooseFileButton);
-        buttons.add(clearButton);
-
-        clearButton.setEnabled(false);
-
-        chosenFileNameTextField.setText(getLabelName());
-        chosenFileNameTextField.setEditable(false);
-
-        chosenFileNameTextField.setDropTarget(new DropTarget() {
-            @SuppressWarnings("unchecked")
-            @Override
-            public synchronized void drop(final DropTargetDropEvent e) {
-                e.acceptDrop(DnDConstants.ACTION_COPY);
-                try {
-                    List<File> droppedFiles = (List<File>) e.getTransferable()
-                            .getTransferData(DataFlavor.javaFileListFlavor);
-                    if (droppedFiles != null && !droppedFiles.isEmpty()) {
-                        setFile(droppedFiles.get(0));
-                    }
-                } catch (Exception ex) {
-                    // ignore
-                }
-            }
-        });
-        chooseFileButton.setDropTarget(chosenFileNameTextField.getDropTarget());
-
-        JPanel paddedTextField = new JPanel(new BorderLayout());
-        chosenFileNameTextField.setMinimumSize(new Dimension(200, chosenFileNameTextField.getMinimumSize().height));
-        paddedTextField.add(chosenFileNameTextField);
-
-        BorderLayoutMaster.addCenterRight(this, paddedTextField, buttons);
-    }
-
-    public void onChooseFileButtonClick(ActionEvent e) {
-        chooseFileButton.setEnabled(false);
-        environment.chooseFileToRead(new Environment.FileCallback() {
-            @Override
-            public void onFileChosen(File file) {
-                setFile(file);
-                chooseFileButton.setEnabled(true);
-            }
-
-            @Override
-            public void onNoFileChosen() {
-                chooseFileButton.setEnabled(true);
-            }
-        }, fileFilters);
-    }
-
-    public void onClearButtonClick(ActionEvent e) {
-        setFile(null);
-    }
-
-    private void setFile(File file) {
-        this.file = file;
-        chosenFileNameTextField.setText(
-                file == null
-                        ? getLabelName()
-                        : file.getName()
-        );
-        clearButton.setEnabled(file != null);
-        fireSubmit();
+        return new FileFilter[0];
     }
 
     @Override
     public void setEnabled(boolean enabled) {
-        chooseFileButton.setEnabled(enabled);
-        clearButton.setEnabled(enabled && file != null);
+        fileChooserInput.setEnabled(enabled);
     }
 
     @Override
     public void applyCurrentValue() throws IllegalAccessException {
-        field.set(target, file);
+        field.set(target, fileChooserInput.getFile().orElse(null));
     }
 }
