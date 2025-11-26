8ea8692c99d3bfedfc4e240e0cfc8ce0fb8954da
Gotterdemarung <gotterdemarung@gmail.com>
2025-11-26 15:17:14 +0200

    :sparkles: Vertical rows panel with no border

--- swing/src/main/java/io/github/mjcro/toybox/swing/widgets/panels/VerticalRowsPanel.java
+++ swing/src/main/java/io/github/mjcro/toybox/swing/widgets/panels/VerticalRowsPanel.java
@@ -3,13 +3,20 @@ package io.github.mjcro.toybox.swing.widgets.panels;
 import io.github.mjcro.toybox.swing.layouts.RowsLayout;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class VerticalRowsPanel extends JPanel {
     public VerticalRowsPanel() {
+        this(new EmptyBorder(10, 10, 10, 10));
+    }
+
+    public VerticalRowsPanel(Border border) {
         setLayout(new RowsLayout());
-        setBorder(new EmptyBorder(10, 10, 10, 10));
+        if (border != null) {
+            setBorder(border);
+        }
     }
 
     @Override
