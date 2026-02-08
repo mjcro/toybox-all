78424c37d022066fe1a83265f090bbfdfb0a313a
gotterdemarung <gotterdemarung@gmail.com>
2026-02-08 10:59:35 +0200

    :sparkles: Simple label

--- swing/src/main/java/io/github/mjcro/toybox/swing/widgets/MultiViewBasicPanel.java
+++ swing/src/main/java/io/github/mjcro/toybox/swing/widgets/MultiViewBasicPanel.java
@@ -6,14 +6,17 @@ import java.awt.*;
 public abstract class MultiViewBasicPanel extends JPanel {
     public static final String
             CARD_EMPTY = "empty",
+            CARD_LABEL = "label",
             CARD_EXCEPTION = "exception";
 
+    private final LabelOnlyJPanel labelOnlyJPanel = new LabelOnlyJPanel();
     private final ExceptionDetailsJPanel exceptionDetailsJPanel = new ExceptionDetailsJPanel();
 
     public MultiViewBasicPanel() {
         super(new CardLayout());
 
         add(new JPanel(), CARD_EMPTY);
+        add(labelOnlyJPanel, CARD_LABEL);
         add(exceptionDetailsJPanel, CARD_EXCEPTION);
     }
 
@@ -25,9 +28,15 @@ public abstract class MultiViewBasicPanel extends JPanel {
         setSelectedCard(CARD_EMPTY);
     }
 
+    public void setViewLabel(String value) {
+        labelOnlyJPanel.label.setText(value);
+        setSelectedCard(CARD_LABEL);
+    }
+
     @Override
     public void setEnabled(boolean enabled) {
         super.setEnabled(enabled);
+        labelOnlyJPanel.setEnabled(enabled);
         exceptionDetailsJPanel.setEnabled(enabled);
     }
 
@@ -37,6 +46,14 @@ public abstract class MultiViewBasicPanel extends JPanel {
         } else {
             exceptionDetailsJPanel.setException(t);
             setSelectedCard(CARD_EXCEPTION);
+        }
+    }
+
+    private static final class LabelOnlyJPanel extends JPanel {
+        private final JLabel label = new JLabel();
+
+        LabelOnlyJPanel() {
+            add(label);
         }
     }
 }
